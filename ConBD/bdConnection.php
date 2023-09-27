4c4b371c7de6972d1511b92de93ecaabd9b9cfa9
Luiz Felipe Gomes de Oliveira Ramires <84055766+kanekiiin@users.noreply.github.com>
2023-09-27 15:47:34 -0300

    Add files via upload

---
+++ ConBD/bdConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+class Connection
+{
+    private static $conn=null;
+    public static function getConnection(){
+        self::$conn = null;
+        if (self::$conn === null) {
+            $options = array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            );
+
+            $host       = 'localhost';
+            $database   = 'bdweb2';
+            $port       = 3306; 
+            $user       = 'root';
+            $password   = 'DarkFireBD17!';
+
+            try { self::$conn = new PDO("mysql:host=$host;dbname=$database;port=$port",
+                    $user, $password, $options);
+                return self::$conn; 
+            } catch (PDOException $e) {
+                echo "Erro in BD: " . $e->getMessage();
+                return null;
+            }
+        }
+        return self::$conn; 
+    }
+}
